b939dd45ea31dae804d1e04e638bfa30fdb8974a
Taras <eostasarat@gmail.com>
2026-09-04 23:40:24 +0300

    fixed, add parametrize, boundaries, and type errors

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,46 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_ages_before_first_human_year() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-    assert get_human_age(14, 14) == [0, 0]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (0, 0, [0, 0]),
+        (-5, -10, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (16, 16, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (25, 25, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (28, 29, [3, 3]),
+        (100, 100, [21, 17]),
+        (1000, 1000, [246, 197]),
+    ]
+)
+def test_get_human_age_valid_inputs(
+    cat_age: int,
+    dog_age: int,
+    expected: list
+) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
 
 
-def test_ages_between_first_and_second_human_year() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_ages_at_exactly_second_human_year() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_ages_after_second_human_year() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        ("15", 15),
+        (15, "15"),
+        ([15], 15),
+        (15, {"age": 15}),
+        (None, 15),
+    ]
+)
+def test_get_human_age_incorrect_types(
+    cat_age: any,
+    dog_age: any
+) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
